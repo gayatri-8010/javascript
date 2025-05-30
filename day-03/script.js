46223be4f6e974fb7f1a3c462bbb0cc198d96de2
gayatri-8010 <gayatrithorat03@gmail.com>
2025-05-30 11:26:33 +0530

    operators

---
+++ day-03/script.js
@@ -0,0 +1,40 @@
+
+// 3. Arithmatic operators
+
+// a. Addition --> +
+
+const num1 = 10;
+
+const num2 = 20;
+
+const addition = num1 + num2;
+
+console.log("Addition is", addition);
+
+// b. Substraction
+
+const subs = num1 - num2;
+
+console.log("Substraction is", subs);
+
+// c. Multiplication
+
+const mult = num1 * num2;
+
+console.log("Multiplication is", mult);
+
+// d. Division
+
+const div = num1 / num2;
+
+console.log('Division is', div);
+
+console.log(0 / 0);
+
+// 3. modulo operator --> remainder
+
+console.log(20 % 10);
+
+// f. Exponent operator
+
+console.log(4 ** 10); // 4 ^ 10
