aa638784f704d001d620b0c642c7ad541c7ce71d
gayatri-8010 <gayatrithorat03@gmail.com>
2025-05-30 11:33:38 +0530

    for loop

---
+++ day-08/script.js
@@ -0,0 +1,55 @@
+//for loop
+// initalisation;condition;updation
+
+for(let num=1;num<=10;num=num+1){
+    console.log("gayatri"+num);
+}
+
+//print no from 1 to 100
+for(let num=1;num<=100;num=num+1)
+    {
+    console.log(num);
+}
+//print no from 0 to -100
+
+for(let num=0;num>=-100;num=num-1)
+    {
+    console.log(num);
+}
+
+
+//for loop for odd numbers
+
+for(let num1=1; num1 <=50 ;num1=num1+2)
+    {
+console.log(num1);
+   }
+//for loop for odd numbers
+for(let num1=1; num1 <=50 ;num1=num1+2){
+if(num1%2==1){
+console.log(num1);
+}
+}
+
+//print 10 table
+for(i=1;i<=10;i++){
+    console.log(10*i);
+}
+
+//print 10 table
+for(i=10;i<=100;i+=10){
+    console.log(i);
+}
+
+
+//print 10 table
+
+for(i=1;i<=10;i++){
+    console.log(`10 x${i}=${10*i} `);
+}
+
+//break--->to stop the loop
+//continue--->to skip the current iteration
+
+//nested for loop
+// how to break from outer loop when we use break statement in inner loop
