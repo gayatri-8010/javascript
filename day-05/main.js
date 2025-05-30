c22b9a3018315e0df4ad91dcc335078189e9ab82
gayatri-8010 <gayatrithorat03@gmail.com>
2025-05-30 11:28:46 +0530

    and or not operators

---
+++ day-05/main.js
@@ -0,0 +1,32 @@
+//AND
+console.log(true&&true);
+console.log(true&&false)
+console.log(false&&true)
+console.log(false&&false)
+
+//OR
+
+console.log(true||true);
+console.log(true||false);
+console.log(false||true);
+console.log(false||false);
+
+//NOT
+console.log(!true);
+console.log(!false);
+
+//ternary operator
+let age=29;
+//age >=18? console.log("you can vote"):console.log("sorry,you can not vote");
+
+
+
+//compound operators
+//+=,-=,*=,/=,%=
+age+=1;
+console.log(age);
+
+//short circuit operators
+//&,|,~  --->returns 1 or 0 as a true and false
+
+//<<,>> --->left shift,right shift operator
